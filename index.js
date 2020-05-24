cd6197bb999b714f60b4dd24a5aeeaf910a138c0
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-05-24 09:00:15 -0700

    revert attempt

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ function begin() {
         //stop default GETTING A NOT DEFINED ERROR
         e.preventDefault();
         console.log("ignition");
+<<<<<<< HEAD
         render(1);
         
+=======
+>>>>>>> parent of e4ad82c... centered text
     });
 }
 
+<<<<<<< HEAD
 function home() {
     return `
         <div class="js-quiz-start">
@@ -48,6 +52,14 @@ function render(textQuestion) {
 function pullQuestion(questionCount) {
     return 
         `<div class="generatedQuestion">
+=======
+function pullQuestion() {
+    //fill in a form with information from list 
+    //create a submit button
+    const questionCount = 0
+    const currentQuestion = $(
+        `<div>
+>>>>>>> parent of e4ad82c... centered text
                 <h2>
                     ${list[questionCount].question}
                 </h2>
@@ -64,28 +76,36 @@ function pullQuestion(questionCount) {
 
                 </form>
                 <p> 
+<<<<<<< HEAD
                     Mission Completion: "test"% <br>
                     "test"
                 </p>
             </div>`
+=======
+                    Mission Completion: ${percentage} <br>
+                    ${quotes[0].quote}
+                </p>
+            </div>`
+    );
+    //return and display %
+    //
+    $("main").html(currentQuestion);
+    //check remaining questions if none call display results function
+    questionCheck();
+    console.log("Question Generated");
+>>>>>>> parent of e4ad82c... centered text
 }
 
 function questionCheck() {
     //listen for form submit button
-    //this event listener is not functional
-    $(document).on('click', '#nextQuestion', function(e){ 
+    $(document).on('click', '.nextQuestion', function(e){ 
         e.preventDefault;
-        questionCount ++; //There is a scope issue here
-        let submittedAwnser = $("input[name=options]:checked").val();
-        alert(submittedAwnser);
-        if (list[questionCount].awnser === submitedAwnser){
-            result ++;
-            console.log("result increased");
-        };
+        questionCount ++;
     });
-    //
-    //alert("questionCheck run")
-    //pullQuestion();
+    if (list[questionCount].awnser === submitedAwnser){
+        result ++;
+    };
+    pullQuestion();
     //check if awnser was correct
     //update percentage
     //call pullQuestion
@@ -101,11 +121,45 @@ function startOver() {
     //call the begin function 
 }
 
+<<<<<<< HEAD
 function run(){
     render(0);
 }
+=======
+>>>>>>> parent of e4ad82c... centered text
 
 
 
+// This is the code used for an example
+function handleSelectOption() {
+    $('body').on("submit",'#js-questions', function(event) {
+      event.preventDefault();
+      let currentQues = STORE.questions[STORE.currentQuestion];
+      let selectedOption = $("input[name=options]:checked").val();
+      if (!selectedOption) {
+        alert("Choose an option");
+        return;
+      } 
+      let id_num = currentQues.options.findIndex(i => i === selectedOption);
+      let id = "#js-r" + ++id_num;
+      $('span').removeClass("right-answer wrong-answer");
+      if(selectedOption === currentQues.answer) {
+        STORE.score++; 
+        $(`${id}`).append(`You got it right<br/>`);
+        $(`${id}`).addClass("right-answer");
+      }
+      else {
+        $(`${id}`).append(`You got it wrong <br/> The answer is "${currentQues.answer}"<br/>`);
+        $(`${id}`).addClass("wrong-answer");
+      }
+  
+      STORE.currentQuestion++;
+      $("#js-score").text(`Score: ${STORE.score}/${STORE.questions.length}`);
+      $('#answer').hide();
+      $("input[type=radio]").attr('disabled', true);
+      $('#next-question').show();
+    });
+  }
 
-$(run); // Test save
+
+$(begin); // Test save
